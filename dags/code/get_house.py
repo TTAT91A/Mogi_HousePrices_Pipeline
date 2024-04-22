6c0247308a0bfa46ff55e76cf331e5922560eb0c
TTAT91A <anhtuan.ltqb@gmail.com>
2024-04-22 22:52:14 +0700

    add

--- dags/code/get_house.py
+++ dags/code/get_house.py
@@ -27,8 +27,9 @@ headers = {
   "Sec-Fetch-Site": "same-origin",
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
 }
+today = datetime.now().date()
+FILE_NAME = f'house({today}).csv'
 
-FILE_NAME = f'house({datetime.now().date()}).csv'
 
 def get_house_link(path):
     df = pd.DataFrame(columns=['name', 'district', 'price', 'bedroom', 'wc', 'acreage', 'link', 'date'])
@@ -98,13 +99,13 @@ def get_all_files(username='TTAT91A', password=TOKEN, repo_name='House_Prices_Pi
             all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
     return all_files
 
-def pushToGithub(local_file_path, username='TTAT91A', password=TOKEN):
+def pushToGithub(local_file_path, username='TTAT91A', password=TOKEN, file_name=FILE_NAME, repo_name='House_Prices_Pipeline'):
     # g = Github(username, password)
     # # repo = g.get_user().get_repo('Mogi_HousePrices_Pipeline')
     # repo = g.get_user().get_repo('House_Prices_Pipeline')
 
-    repo = connect_github(username, password, repo_name='Mogi_HousePrices_Pipeline')
-    all_files = get_all_files(username, password, repo_name='Mogi_HousePrices_Pipeline')
+    repo = connect_github(username, password, repo_name=repo_name)
+    all_files = get_all_files(username, password, repo_name=repo_name)
 
     if os.path.exists(local_file_path):
         with open(local_file_path, 'r', encoding='utf-8') as file:
@@ -114,21 +115,24 @@ def pushToGithub(local_file_path, username='TTAT91A', password=TOKEN):
         return
 
     # Upload to github
-    git_file = 'dags/data/' + FILE_NAME #check file in repo
+    git_file = 'dags/data/' + file_name #check file in repo
     if git_file in all_files:
         contents = repo.get_contents(git_file)
-        commit = "Upload file " + str(get_date())
+        commit = "Upload file " + str(today)
         repo.update_file(contents.path, commit, content, contents.sha, branch="main")
         print(git_file + ' UPDATED')
     else:
-        repo.create_file(git_file, "committing files", content, branch="main")
+        commit = "Upload file " + str(today)
+        repo.create_file(git_file, commit, content, branch="main")
         print(git_file + ' CREATED')
-    
 
 if __name__ == '__main__':
     folder_path = os.path.join(os.path.dirname(__file__))
     dags_folder = os.path.dirname(folder_path)
     today = str(get_date())
-    dest_path = dags_folder + f"/data/" + FILE_NAME
-    get_house_link(dest_path) #write to file
-    pushToGithub(dest_path)
+
+    file_name = 'house(2024-04-19) copy.csv'
+    dest_path = dags_folder + f"/data/" + file_name
+    # get_house_link(dest_path) #write to file
+    pushToGithub(local_file_path=dest_path, file_name=file_name, repo_name='Mogi_HousePrices_Pipeline')
+    # print(get_all_files(repo_name="Mogi_HousePrices_Pipeline"))
