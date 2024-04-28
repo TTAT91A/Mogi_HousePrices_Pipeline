0718a56fc8859cbd9360206aafc423befa0790bc
TTAT91A <anhtuan.ltqb@gmail.com>
2024-04-28 21:17:39 +0700

    add

--- dags/code/Save_to_MongoDB.py
+++ dags/code/Save_to_MongoDB.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import os
 # from pre_processing import *
 
-def import_csv_to_mongodb(df, collection_name, database_name='House_prices', mongo_uri='mongodb+srv://nattan1811:taibitri123@cluster0.voqacs7.mongodb.net/'):
+def import_csv_to_mongodb(df, database_name='House_prices', collection_name = "HCMCity", mongo_uri='mongodb+srv://nattan1811:taibitri123@cluster0.voqacs7.mongodb.net/'):
 # mongodb+srv://nattan1811:<password>@cluster0.voqacs7.mongodb.net/
     # Read CSV into a Pandas DataFrame
 
@@ -16,8 +16,12 @@ def import_csv_to_mongodb(df, collection_name, database_name='House_prices', mon
     # Convert DataFrame to dictionary for easier MongoDB insertion
     data = df.to_dict(orient='records')
 
-    # Insert data into MongoDB
-    collection.insert_many(data)
+    try:
+        # Insert data into MongoDB
+        collection.insert_many(data)
+        print("Insert data to MongoDB successfully")
+    except:
+        print("Insert data to MongoDB failed")
 
     # Close MongoDB connection
     client.close()
@@ -26,11 +30,11 @@ def get_date():
     return datetime.now().date()
 
 if __name__ == "__main__":
-    collection_name = "HCMCity"
+    # collection_name = "HCMCity"
     today = get_date()
     overpass_name = f'overpass({today}).csv'
 
     overpass_path = "https://raw.githubusercontent.com/TTAT91A/Mogi_HousePrices_Pipeline/main/dags/data1/" + overpass_name
 
     df = pd.read_csv(overpass_path)
-    import_csv_to_mongodb(df, collection_name)
+    import_csv_to_mongodb(df)
